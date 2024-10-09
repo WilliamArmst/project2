d7170c381565b4fa1c970a15bab398ad055e7016
William Armstrong <william_armstrong@mines.edu>
2024-10-09 16:11:05 -0600

    splitpath without node return

--- mkdir_splitpath.c
+++ mkdir_splitpath.c
@@ -1,5 +1,3 @@
-
-
 #include "types.h"
 
 extern struct NODE* root;
@@ -7,10 +5,6 @@ extern struct NODE* cwd;
 
 //make directory
 void mkdir(char pathName[]){
-
-    // TO BE IMPLEMENTED
-    //
-    // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
 
     printf("TO BE IMPLEMENTED\n");
 
@@ -20,12 +14,61 @@ void mkdir(char pathName[]){
 //handles tokenizing and absolute/relative pathing options
 struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
 
-    // TO BE IMPLEMENTED
     // NOTE THAT WITHOUT COMPLETING THIS FUNCTION CORRECTLY
     // rm, rmdir, ls, cd, touch COMMANDS WILL NOT EXECUTE CORRECTLY
     // SEE THE PROVIDED EXECUTABLE TO SEE THEIR EXPECTED BEHAVIOR
 
-    // YOUR CODE HERE
-    //
-    return NULL;
+    // get pathName size
+    unsigned int size = 0;
+    while (pathName[size] != '\0') {
+        size++;
+    }
+
+    // handle edge cases
+    if (size == 0) {
+        dirName[0] = '/';
+        return root;
+    }
+
+    int slashPos = size; // from the end
+    
+    for (int i = size - 1; i >= 0; i--) {
+        if (pathName[i] == '/') { // mark the location of the first slash and break
+            slashPos = -(i - size);
+            break;
+        } 
+    }
+
+    int i;
+    // hopefully memory management is done in the other parts?
+    // dirName = (char*)malloc ((size - slashPos) * sizeof(char));
+    // baseName = (char*)malloc (slashPos * sizeof(char));
+
+    dirName[0] = '\0';
+    baseName[0] = '\0';
+
+    for (i = 0; i < size - slashPos; i++) { // get the directoy name, everything until the slash if there is one
+        dirName[i] = pathName[i]; 
+    }
+
+    if (size != slashPos) { // skip the slash if it's there
+        i++;
+    }
+
+    for (int j = 0; i < size; j++) { // continue loop to get base name, everything until the end
+        baseName[j] = pathName[i];
+        i++;
+    }
+
+    printf("%s\n",dirName);
+    printf("%s\n",baseName);
+
+    if (dirName[0] == '\0') {
+        return cwd;
+    }
+
+    struct NODE* currentDir = cwd;
+
+    return currentDir;
 }
+
